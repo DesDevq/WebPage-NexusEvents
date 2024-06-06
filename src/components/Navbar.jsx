a322484c07a7999d0bf657d8fdcbb44b9c06bd4f
Des <ajjjjok5@gmail.com>
2024-06-06 09:30:02 -0700

    [FEAT] Home webpage finished.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,16 +8,29 @@ export const Navbar = () => {
   <div className='logo'>
     <img src='./Untitled design.png' alt='Logo' className='Logo-css'></img>
   </div>
-     <h1 className='NexusEvents'>Nexus Events</h1>
+     <h1 className='NexusEvents'>NEXUS EVENTS</h1>
        <div className='Nav-links-container'>
      <ul className='Nav-links'>
-        <li ><a href='#Inicio'>Inicio</a></li>
-        <li ><a href='#Servicios'>Servicios</a></li>
-        <li ><a href='#Nosotros'>Nosotros</a></li>
-        <li ><a href='#Contacto'>Contacto</a></li>
+        <li ><a href='#Inicio'>INICIO</a></li>
+        <li ><a href='#Servicios'>SERVICIOS</a></li>
+        <li ><a href='#Nosotros'>NOSOTROS</a></li>
+        <li ><a href='#Contacto'>CONTACTO</a></li>
      </ul>
      </div>
     </div>
+    <div className='image-container'>
+        <img src='./fotofondo7.jpeg' alt='Below Navbar' className='below-navbar-image' />
+      </div>
+      <div className='image-overlay'></div>
+        <div className='Content'>
+            <h3><strong>TRANFORMAMOS IDEAS EN <br></br> EXPERIENCIAS
+                MEMORABLES</strong>
+            </h3>
+            <p className='Content-2'><strong>Nexus Events la mejor opcion para la decoracion <br></br>de eventos
+              empresariales</strong>
+            </p>
+            <a href='' className='hero-btn'>Contactanos</a>
+        </div>
     </>
   )
 }
